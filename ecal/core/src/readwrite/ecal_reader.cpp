109e88485e15a038629bc174e642cb12edacf68c
Orhan Kupusoglu <orhan.kupusoglu@gmail.com>
2022-06-18 03:11:42 +0200

    Test CPP changes

--- ecal/core/src/readwrite/ecal_reader.cpp
+++ ecal/core/src/readwrite/ecal_reader.cpp
@@ -5,17 +5,17 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * TEST CLANG-TIDY
  * ========================= eCAL LICENSE =================================
-*/
+ */
 
 /**
  * @brief  common eCAL data reader
@@ -180,7 +180,7 @@ namespace eCAL
 
     return(true);
   }
-    
+
   void CDataReader::InitializeLayers()
   {
     // start ecal udp multicast layer
@@ -234,7 +234,7 @@ namespace eCAL
       CInProcLayer::Get()->AddSubscription(m_host_name, m_topic_name, m_topic_id, m_qos);
     }
   }
-  
+
   void CDataReader::StopDataLayers()
   {
     // stop ecal udp multicast layer
@@ -747,7 +747,7 @@ namespace eCAL
       m_writer_counter_map[tid_] = counter_;
     }
   }
-    
+
   void CDataReader::RefreshRegistration()
   {
     if(!m_created) return;
