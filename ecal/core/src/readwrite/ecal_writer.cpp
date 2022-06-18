fb34b4fc96c93fd94b25f7723605c686e863960b
Orhan Kupusoglu <orhan.kupusoglu-ext@continental.corporation.com>
2022-06-19 01:21:03 +0200

    Test CPP changes 13

--- ecal/core/src/readwrite/ecal_writer.cpp
+++ ecal/core/src/readwrite/ecal_writer.cpp
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * TEST CLANG-TIDY-12
+ * TEST CLANG-TIDY-13
  * ========================= eCAL LICENSE =================================
 */
 
